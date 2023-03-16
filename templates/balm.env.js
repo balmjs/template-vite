35081da05590555da4e6d0a75e353cf47e799b21
elf-mouse <ifmouz@gmail.com>
2023-03-16 13:47:20 +0800

    chore: update

---
+++ templates/balm.env.js
@@ -0,0 +1,7 @@
+process.env.BALM_GIT_FLOW_MAIN = 'main'
+process.env.BALM_GIT_FLOW_RELEASE = 'release'
+process.env.BALM_GIT_FLOW_RELEASES = ['release']
+process.env.BALM_GIT_FLOW_SCRIPTS = ['build']
+process.env.BALM_GIT_FLOW_BUILD_DIR = 'dist'
+// process.env.BALM_GIT_FLOW_REPOSITORIES = [];
+// process.env.BALM_GIT_FLOW_SITE = '';
